b542f2d4cb8a0f571148256d7a6cc2c8701fb231
aakumar07 <126548776+aakumar07@users.noreply.github.com>
2023-09-22 20:04:36 +0530

    Update mvn.sh

--- mvn.sh
+++ mvn.sh
@@ -6,6 +6,3 @@ git clone https://github.com/aakumar07/maven.git
 wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
 sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
 yum install -y apache-maven
-
-cd /home/ec2-user/maven/new_web/
-mvn clean package
